2a4af17d07b04e7ae1972b6c2314c7e6a03b66c3
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2016-01-08 11:40:10 +0100

    cli: add argument parser

--- src/nolimia32.cc
+++ src/nolimia32.cc
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/SourceMgr.h>
+
+static llvm::cl::opt<std::string>
+input_filename(llvm::cl::Positional, llvm::cl::desc("<input file>"),
+               llvm::cl::init("-"));
 
 int main(int argc, char const *argv[])
 {
-  std::cout << "error" << std::endl;
-  return 1;
+  llvm::cl::ParseCommandLineOptions(argc, argv, "nolimia32\n");
+
+  auto buffer_ptr = llvm::MemoryBuffer::getFileOrSTDIN(input_filename);
+  if (auto ec = buffer_ptr.getError())
+  {
+    llvm::errs() << input_filename << ": " << ec.message() << '\n';
+    return 1;
+  }
+
+  llvm::outs() << "Processing file: " << input_filename << '\n';
+
+  return 0;
 }
