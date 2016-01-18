1d2651b09a495a98552d12dddd00d2703dabfdd0
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2016-01-18 12:09:42 +0100

    tests: generate correct tests for binary instructions

--- tests/gen-test.py
+++ tests/gen-test.py
@@ -4,7 +4,8 @@
 # a file named mov-rt contains the following instruction:
 # mov %eax, %t0
 
-binary_instructions = ["mov"]
+binary_instructions = ["mov", "add", "imul", "lea", "sal", "sar", "sub"]
+need_suffix = ["mov", "add", "sub"]
 operands_types = ["r", "i", "m", "t"]
 operands_for_type = {
         "r" : "%eax", # register
@@ -13,19 +14,36 @@ operands_for_type = {
         "t" : "%t0", # temporary
         }
 
-def valid_types(lhs, rhs):
+def valid_types(instr, lhs, rhs):
     if rhs == "i":
         return False
+    if rhs == "m" and lhs == "m":
+        return False
+    if instr == "lea" and (lhs != "m" or rhs != "r"):
+        return False
+    if instr == "imul" and rhs == "m":
+        return False
+    if instr == "sal" and (lhs != "i" or (rhs != "r" or rhs != "m")):
+        return False
+    if instr == "sar" and (lhs != "i" or (rhs != "r" or rhs != "m")):
+        return False
+
     return True
+
+def add_instr_suffix(instr, lhs, rhs):
+    if (rhs == "m" or rhs == "t") and instr in need_suffix:
+        return instr + "l"
+    return instr
 
 for instr in binary_instructions:
     for lhs in operands_types:
         for rhs in operands_types:
-            if not(valid_types(lhs, rhs)):
+            if not(valid_types(instr, lhs, rhs)):
                 continue
 
-            filename = "{0}-{1}{2}.s".format(instr, lhs, rhs)
+            suff_instr = add_instr_suffix(instr, lhs, rhs)
+            filename = "{0}-{1}{2}.s".format(suff_instr, lhs, rhs)
             file = open(filename, "w")
             file.write("  .text\n") # Section name
-            file.write("{0} {1}, {2}".format(instr, operands_for_type[lhs],
+            file.write("{0} {1}, {2}".format(suff_instr, operands_for_type[lhs],
                                                     operands_for_type[rhs]))
