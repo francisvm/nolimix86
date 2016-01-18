448643e35ae0cd4a1f7868ee6899a8896af07e09
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2016-01-18 16:48:22 +0100

    ast: add missing fwd declarations

--- src/ast/fwd.hh
+++ src/ast/fwd.hh
@@ -10,12 +10,73 @@ namespace nolimix86
 
     class ast;
 
+    template <template <typename> class Const>
+    class visitor_impl;
+
     class instr_base;
 
     template <opcode_t opcode, size_t operands_count>
     class instr;
 
     class operand;
+
+    class add;
+
+    class call;
+
+    class cmp;
+
+    class idiv;
+
+    class imul;
+
+    class ja;
+
+    class jae;
+
+    class jb;
+
+    class jbe;
+
+    class je;
+
+    class jg;
+
+    class jge;
+
+    class jl;
+
+    class jle;
+
+    class jmp;
+
+    class jne;
+
+    class js;
+
+    class lea;
+
+    class leave;
+
+    class mov;
+
+    class neg;
+
+    class pop;
+
+    class push;
+
+    class ret;
+
+    class sal;
+
+    class sar;
+
+    class sete;
+
+    class sub;
+
+    class test;
 
   } // namespace ast
 
