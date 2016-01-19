6f333913efc946044376cd89c6e8595dcc586b05
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2016-01-20 00:34:37 +0100

    ast: add one file that includes them all

---
+++ src/ast/all.hh
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <ast/fwd.hh>
+#include <ast/ast.hh>
+#include <ast/basic-block.hh>
+#include <ast/instr-base.hh>
+#include <ast/instr.hh>
+#include <ast/instructions.hh>
+#include <ast/operand.hh>
