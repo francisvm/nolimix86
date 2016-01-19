f68b684c60d02a889ac904868b1e7c918791acb1
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2016-01-20 00:35:24 +0100

    ast: add visit functions for all the instructions

--- src/ast/visitor.hh
+++ src/ast/visitor.hh
@@ -29,6 +29,66 @@ namespace nolimix86
         virtual void operator()(const_t<instr_base>&);
 
         virtual void operator()(const_t<operand>&);
+
+        virtual void operator()(const_t<add>&) = 0;
+
+        virtual void operator()(const_t<call>&) = 0;
+
+        virtual void operator()(const_t<cmp>&) = 0;
+
+        virtual void operator()(const_t<idiv>&) = 0;
+
+        virtual void operator()(const_t<imul>&) = 0;
+
+        virtual void operator()(const_t<ja>&) = 0;
+
+        virtual void operator()(const_t<jae>&) = 0;
+
+        virtual void operator()(const_t<jb>&) = 0;
+
+        virtual void operator()(const_t<jbe>&) = 0;
+
+        virtual void operator()(const_t<je>&) = 0;
+
+        virtual void operator()(const_t<jg>&) = 0;
+
+        virtual void operator()(const_t<jge>&) = 0;
+
+        virtual void operator()(const_t<jl>&) = 0;
+
+        virtual void operator()(const_t<jle>&) = 0;
+
+        virtual void operator()(const_t<jmp>&) = 0;
+
+        virtual void operator()(const_t<jne>&) = 0;
+
+        virtual void operator()(const_t<js>&) = 0;
+
+        virtual void operator()(const_t<lea>&) = 0;
+
+        virtual void operator()(const_t<leave>&) = 0;
+
+        virtual void operator()(const_t<mov>&) = 0;
+
+        virtual void operator()(const_t<neg>&) = 0;
+
+        virtual void operator()(const_t<pop>&) = 0;
+
+        virtual void operator()(const_t<push>&) = 0;
+
+        virtual void operator()(const_t<ret>&) = 0;
+
+        virtual void operator()(const_t<sal>&) = 0;
+
+        virtual void operator()(const_t<sar>&) = 0;
+
+        virtual void operator()(const_t<sete>&) = 0;
+
+        virtual void operator()(const_t<sub>&) = 0;
+
+        virtual void operator()(const_t<test>&) = 0;
+
+        virtual void operator()(const_t<basic_block>&) = 0;
     };
 
     /// Const visitor: does not alter the AST.
