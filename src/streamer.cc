dd4ab3b147eb1f0071aa74852b10e2a4cf16deb7
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2016-01-24 02:01:31 +0100

    asm-parser: adapt parser to MCInst lowering

--- src/streamer.cc
+++ src/streamer.cc
@@ -26,7 +26,13 @@ namespace nolimix86
         return ast::make_operand<ast::operand::imm_tag>(
           static_cast<size_t>(op.getImm()));
       else
-        return ast::operand{10}; // FIXME: Handle all the operands.
+        assert(!"Unknown operand.");
+    }
+
+    ast::operand
+    emit_operand(size_t offset, enum x86::reg reg)
+    {
+      return ast::make_operand<ast::operand::mem_tag>(offset, reg);
     }
 
     // Declaration of the generic visitor dispatcher.
@@ -62,8 +68,29 @@ namespace nolimix86
     visit_instr::visit(ast::apply_visitor<visit_instr>& v_g, Instr& e)
     {
       auto& v = static_cast<instr_operand_emitter&>(v_g);
-      e.set_operand(0, emit_operand(v.inst_.getOperand(2)));
-      e.set_operand(1, emit_operand(v.inst_.getOperand(1)));
+      // Registers, temporaries and immediates.
+      if (v.inst_.size() == 3)
+      {
+        // [dst][dst][src]
+        e.set_operand(0, emit_operand(v.inst_.getOperand(2)));
+        e.set_operand(1, emit_operand(v.inst_.getOperand(1)));
+      }
+      else if (v.inst_.size() == 6) // Memory as a destination.
+      {
+        // [reg][?][?][offset][?][src]
+        e.set_operand(0, emit_operand(v.inst_.getOperand(5)));
+        size_t offset = v.inst_.getOperand(3).getImm();
+        auto reg = static_cast<enum x86::reg>(v.inst_.getOperand(0).getReg());
+        e.set_operand(1, emit_operand(offset, reg));
+      }
+      else if (v.inst_.size() == 7) // Memory as a source.
+      {
+        // [dst][dst][reg][?][?][offset][?]
+        size_t offset = v.inst_.getOperand(5).getImm();
+        auto reg = static_cast<enum x86::reg>(v.inst_.getOperand(2).getReg());
+        e.set_operand(0, emit_operand(offset, reg));
+        e.set_operand(1, emit_operand(v.inst_.getOperand(0)));
+      }
     }
 
     // Handle the rest.
